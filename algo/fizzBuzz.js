b87a0dd061f4512ea41e9dae4f7b031d7914c35d
thibault.leger <thibault.leger.a@gmail.com>
2023-07-25 15:23:53 +0200

    Improve fizzbuzz algorithm for better visualisation, fix parameter usage

--- algo/fizzBuzz.js
+++ algo/fizzBuzz.js
@@ -1,4 +1,4 @@
-let n = process.argv[2];
+let n = parseInt(process.argv[2]);
 
 // default value is no param specified
 if (!n || Number.isNaN(n) || !Number.isInteger(n) ) {
@@ -6,8 +6,6 @@ if (!n || Number.isNaN(n) || !Number.isInteger(n) ) {
 }
 
 function fizzBuzz(n) {
-    console.log(n);
-
     let string = "";
 
     for (let i = 1; i <= n; i++) {
@@ -25,9 +23,11 @@ function fizzBuzz(n) {
        if (!divisibleBy3 && !divisibleBy5) {
         string += `${i}`;
        }
+
+       string += `\n`;
     }
 
-    return string;
+    console.log(string);
 }
 
-console.log(fizzBuzz(n));
+fizzBuzz(n);
