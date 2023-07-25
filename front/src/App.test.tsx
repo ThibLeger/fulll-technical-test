b52c53839768d19e2cfc99efdfb50dc7ca71e228
thibault.leger <thibault.leger.a@gmail.com>
2023-07-25 11:05:57 +0200

    Add some tests, improve readme

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Github Search/i);
+  expect(title).toBeInTheDocument();
+});
+
+test('the search bar is present', () => {
+  render(<App />);
+  const searchInput = screen.getByPlaceholderText(/search input/i);
+  expect(searchInput).toBeInTheDocument();
+});
+
+test('no result by default', () => {
+  render(<App />);
+  const noResultText = screen.getByText(/no result/i);
+  expect(noResultText).toBeInTheDocument();
+});
+
+test('search a user and check its checkbox', () => {
+  const { container } = render(<App />);
+  const searchInput = screen.getByPlaceholderText(/search input/i);
+  
+  fireEvent.change(searchInput, { target: {value: "ThibLeger"} });
+
+  // wait for debounce and api
+  setTimeout(() => {
+    const userId = screen.getByText(/14903853/i);
+    expect(userId).toBeInTheDocument();
+
+    const userCheckbox = container.getElementsByClassName("userCardCheckbox")[0];
+    expect(userCheckbox).toBeInTheDocument();
+
+    fireEvent.click(userCheckbox);
+    const nbOfElements = screen.getByText(/1 element selected/i);
+    expect(nbOfElements).toBeInTheDocument();
+  }, 1000);
 });
